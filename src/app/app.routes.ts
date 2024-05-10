4f0bd34aac365e7e027ff03c925963bf39e6b1dc
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-05-10 10:55:09 -0500

    creación de componente ejercicio3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Ejercicio3Component } from './ejercicio3/ejercicio3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'ejercicio3', component: Ejercicio3Component}
+];
