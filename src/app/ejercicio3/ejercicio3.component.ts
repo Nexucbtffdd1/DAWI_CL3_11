65db021cb98000bf9bd90fbf0ac4ae91ac32c38c
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-05-10 12:49:31 -0500

    archivo ejercicio3.component.ts implementado

--- src/app/ejercicio3/ejercicio3.component.ts
+++ src/app/ejercicio3/ejercicio3.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio3',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio3.component.html',
   styleUrl: './ejercicio3.component.scss'
 })
 export class Ejercicio3Component {
+  calificaciones= {
+    calificacion1: 0,
+    calificacion2: 0,
+    calificacion3: 0,
+    resultado: 0
 
+  }
+  mostrarResultado = false;
+  mensaje: string ='';
 }
